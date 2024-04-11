eb209ce31f5c61e4d574f1cac6f4df297ccac439
Samms206 <sh2069936@gmail.com>
2024-04-11 14:48:05 +0700

    addAdminController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminColtroller;
 
-Route::get('/', function () {
-    return view('dashboard');
-});
+// Route::get('/', function () {
+//     return view('index');
+// });
+
+Route::get('/', [AdminColtroller::class, 'index']);
