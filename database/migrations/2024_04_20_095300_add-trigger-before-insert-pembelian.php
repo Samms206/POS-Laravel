297c8cc425e260987df53c832932b3cd9f70b395
Samms206 <sh2069936@gmail.com>
2024-04-20 16:55:56 +0700

    addded-trigger-before_insert_pembelian

---
+++ database/migrations/2024_04_20_095300_add-trigger-before-insert-pembelian.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::unprepared('
+            CREATE TRIGGER before_insert_pembelian
+            BEFORE INSERT ON detail_supliers
+            FOR EACH ROW
+            BEGIN
+                DECLARE stok_awal INT;
+                SELECT stok INTO stok_awal FROM barangs WHERE id_brg = NEW.id_barang;
+                SET NEW.stok_sebelum = stok_awal;
+                SET NEW.stok_sesudah = stok_awal + NEW.tambah_stok;
+            END
+        ');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::unprepared('DROP TRIGGER IF EXISTS before_insert_pembelian');
+    }
+};
