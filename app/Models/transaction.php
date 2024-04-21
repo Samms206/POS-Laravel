6a14ed7337bcbfc6cb5bedbdc76e62b5d57c8ab4
Samms206 <sh2069936@gmail.com>
2024-04-21 13:54:14 +0700

    updated-eloquent-relationship-transactions

--- app/Models/transaction.php
+++ app/Models/transaction.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Transaction extends Model
 {
     use HasFactory;
+
+    public function staff()
+    {
+        return $this->belongsTo(User::class, 'id_staff', 'id');
+    }
+
+    public function detailTransactions()
+    {
+        return $this->hasMany(DetailTransaction::class, 'id_transaction', 'id');
+    }
 }
