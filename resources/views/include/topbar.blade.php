95d279574a4728fef386da956c84a1ce71217f8b
Samms206 <sh2069936@gmail.com>
2024-04-22 23:36:10 +0700

    updared-topbar

--- resources/views/include/topbar.blade.php
+++ resources/views/include/topbar.blade.php
@@ -4,27 +4,6 @@
         <i class="fa fa-bars"></i>
     </button>
     <ul class="navbar-nav ml-auto">
-        <li class="nav-item dropdown no-arrow">
-            <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button"
-                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="fas fa-search fa-fw"></i>
-            </a>
-            <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
-                aria-labelledby="searchDropdown">
-                <form class="navbar-search">
-                    <div class="input-group">
-                        <input type="text" class="form-control bg-light border-1 small"
-                            placeholder="What do you want to look for?" aria-label="Search"
-                            aria-describedby="basic-addon2" style="border-color: #3f51b5;">
-                        <div class="input-group-append">
-                            <button class="btn btn-primary" type="button">
-                                <i class="fas fa-search fa-sm"></i>
-                            </button>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </li>
         <li class="nav-item dropdown no-arrow mx-1">
             <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button"
                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -115,60 +94,12 @@
                     Messages</a>
             </div>
         </li>
-        <li class="nav-item dropdown no-arrow mx-1">
-            <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button"
-                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="fas fa-tasks fa-fw"></i>
-                <span class="badge badge-success badge-counter">3</span>
-            </a>
-            <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                aria-labelledby="messagesDropdown">
-                <h6 class="dropdown-header">
-                    Task
-                </h6>
-                <a class="dropdown-item align-items-center" href="#">
-                    <div class="mb-3">
-                        <div class="small text-gray-500">Design Button
-                            <div class="small float-right"><b>50%</b></div>
-                        </div>
-                        <div class="progress" style="height: 12px;">
-                            <div class="progress-bar bg-success" role="progressbar" style="width: 50%"
-                                aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                    </div>
-                </a>
-                <a class="dropdown-item align-items-center" href="#">
-                    <div class="mb-3">
-                        <div class="small text-gray-500">Make Beautiful Transitions
-                            <div class="small float-right"><b>30%</b></div>
-                        </div>
-                        <div class="progress" style="height: 12px;">
-                            <div class="progress-bar bg-warning" role="progressbar" style="width: 30%"
-                                aria-valuenow="30" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                    </div>
-                </a>
-                <a class="dropdown-item align-items-center" href="#">
-                    <div class="mb-3">
-                        <div class="small text-gray-500">Create Pie Chart
-                            <div class="small float-right"><b>75%</b></div>
-                        </div>
-                        <div class="progress" style="height: 12px;">
-                            <div class="progress-bar bg-danger" role="progressbar" style="width: 75%"
-                                aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                    </div>
-                </a>
-                <a class="dropdown-item text-center small text-gray-500" href="#">View All
-                    Taks</a>
-            </div>
-        </li>
         <div class="topbar-divider d-none d-sm-block"></div>
         <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <img class="img-profile rounded-circle" src="img/boy.png" style="max-width: 60px">
-                <span class="ml-2 d-none d-lg-inline text-white small">Maman Ketoprak</span>
+                <span class="ml-2 d-none d-lg-inline text-white small">Sams</span>
             </a>
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                 aria-labelledby="userDropdown">
